faac675b0ac3722d9f2d58f3939477ab8f222cf9
snowman07 <59627816+snowman07@users.noreply.github.com>
2021-02-01 00:50:16 -0700

    index try to show more img

--- week03-04-relational-db-and-authenticated-app/lab2/index.php
+++ week03-04-relational-db-and-authenticated-app/lab2/index.php
@@ -18,6 +18,41 @@
 		<?php 
 
 
+			// $userresult = mysqli_query($con, "SELECT DISTINCT author_id, lab2_users.user_name FROM lab2_mugallery JOIN lab2_users ON lab2_users.user_id = lab2_mugallery.author_id ") or die(mysqli_error($con)); //JOIN lab2_users
+
+			// while($row = mysqli_fetch_array($userresult)){
+
+			// 	$username = $row['user_name'];
+			// 	//echo "Uploaded By: $username <br/>";
+			// 	//echo "<style background-color: green;>";
+			// 	echo "<div style=\"background-color: #2793E7; padding:10px;\" class=\"uploadedby\">Uploaded By: $username </div>";
+			// 	//echo "</style>";
+
+
+			// 	$authorid = $row['author_id'];
+
+			// 	$result = mysqli_query($con, "SELECT * FROM lab2_mugallery WHERE author_id = $authorid") or die(mysqli_error($con));	
+			// 	// LIMIT 3
+
+			// 	while($row = mysqli_fetch_array($result)){
+						
+			// 		$filename =  $row['filename'];
+			// 		$title =  $row['title'];
+			// 		$id =  $row['id'];
+					
+			// 		echo "\n<div class=\"thumb\">";
+			// 		echo "\n\t<a href=\"display.php?id=$id\"><img src=\"images/thumbs-square/$filename\" class=\"img-thumbnail\"></a>";
+			// 		echo "<div class=\"thumb-title\">$title</div>";
+			// 		echo "\n</div>";		
+			// 	}
+				
+			// 	echo "<br style=\"clear:both\"><br /><br /><br />";
+			// 	//echo "<br>";
+			// }
+
+
+			// =========================
+
 			$userresult = mysqli_query($con, "SELECT DISTINCT author_id, lab2_users.user_name FROM lab2_mugallery JOIN lab2_users ON lab2_users.user_id = lab2_mugallery.author_id ") or die(mysqli_error($con)); //JOIN lab2_users
 
 			while($row = mysqli_fetch_array($userresult)){
@@ -49,6 +84,9 @@
 				echo "<br style=\"clear:both\"><br /><br /><br />";
 				//echo "<br>";
 			}
+
+			// =========================
+
 		?>
 		
 		<br style="clear:both">

d19783a755135a251a700eda47fb17f8f422affd
snowman07 <59627816+snowman07@users.noreply.github.com>
2021-02-02 08:23:29 -0700

    db modif done

--- week05-ajax/01_ajaxdemo/admin/dbconnect.php
+++ week05-ajax/01_ajaxdemo/admin/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 
 // PUT YOUR DB CONNECTION INFO IN HERE; USED BY ALL PAGES.
-$con = mysqli_connect("localhost","username","password","db-name") or die ("cannot connect!");
+$con = mysqli_connect("localhost","adomingo4","PDsbNesMr3sSS79","adomingo4_dmit2503") or die ("cannot connect!");
 
 ?>

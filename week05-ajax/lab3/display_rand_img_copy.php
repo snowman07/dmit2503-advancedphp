e5cd6bcd51cd4c5ca7cadbdc512c2f87d69e55c2
snowman07 <59627816+snowman07@users.noreply.github.com>
2021-02-08 23:05:55 -0700

    part c sucks really

--- week05-ajax/lab3/display_rand_img_copy.php
+++ week05-ajax/lab3/display_rand_img_copy.php
@@ -10,34 +10,36 @@
 
 	// FILTERING YOUR DB: On your own....If $_GET vars are present, please use them to filter your DB results (genre, year, decade, label). You can just redefine the previous query.
 	
-	$displayby = $_REQUEST['displayby']; 
-	$displayvalue = $_REQUEST['displayvalue']; 
+	// $displayby = $_REQUEST['displayby']; 
+	// $displayvalue = $_REQUEST['displayvalue']; 
 
-	if (isset($displayby) && isset($displayvalue)) {
+	// if (isset($displayby) && isset($displayvalue)) {
 		
-		// test with "display.php?displayby=genre&displayvalue=jazz" 
+	// 	// test with "display.php?displayby=genre&displayvalue=jazz" 
 
-		$result = mysqli_query($con,"SELECT * FROM cd_catalog_class ORDER BY RAND() LIMIT 1") or die (mysqli_error($con));
+	// 	$result = mysqli_query($con,"SELECT * FROM cd_catalog_class ORDER BY RAND() LIMIT 1") or die (mysqli_error($con));
 
-	}
+	// }
+
+	$result = mysqli_query($con,"SELECT * FROM cd_catalog_class ORDER BY RAND() LIMIT 1") or die (mysqli_error($con));
 
 	while ($row = mysqli_fetch_array($result)) {
 		/// This should out put artist names: On your own....create thumbnail views with images, album names, etc.
-		$artist = $row['artist'];
-		$title = $row['title'];
+		//$artist = $row['artist'];
+		//$title = $row['title'];
 		$artwork = $row['artwork'];
 		
-		echo "<div class=\"thisCD\">\n";
+		//echo "<div class=\"thisCD\">\n";
 
 		echo "<img src=\"artwork/thumbs100/$artwork\" class=\"cdImage\">";
 		
-		echo "<span class=\"displayInfo\">". $artist ."</span><br />\n";
+		//echo "<span class=\"displayInfo\">". $artist ."</span><br />\n";
 
-		echo "<span class=\"displayInfo\"><b>". $title ."</b></span><br />\n";
+		//echo "<span class=\"displayInfo\"><b>". $title ."</b></span><br />\n";
 
-		echo "<br style=\"clear:both\">";
+		//echo "<br style=\"clear:both\">";
 
-		echo "\n</div>\n";
+		//echo "\n</div>\n";
 	}
 
 ?>

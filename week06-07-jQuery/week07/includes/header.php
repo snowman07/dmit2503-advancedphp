51c49483947d1a177f61afb17b4ba9b967e0b9b0
snowman07 <59627816+snowman07@users.noreply.github.com>
2021-02-23 08:23:46 -0700

    week 7 lesson

---
+++ week06-07-jQuery/week07/includes/header.php
@@ -0,0 +1,23 @@
+<?php
+include("mysql_connect.php");
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+<head>
+<title>Blog</title>
+
+<link rel="stylesheet" type="text/css" href="includes/style.css">
+<!--  jQuery goes here -->
+ 
+</head>
+<body>
+<div id="container">
+<div id="banner"> 
+ Blog
+</div>
+
+<div id="content">
+<a href="index.php">All Posts</a>

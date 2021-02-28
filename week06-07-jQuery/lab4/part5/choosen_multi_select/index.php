4d2525f20293222866965c2288c75d11ca0e6367
snowman07 <59627816+snowman07@users.noreply.github.com>
2021-02-28 11:21:28 -0700

    part 5 to be continued

--- week06-07-jQuery/lab4/part5/choosen_multi_select/index.php
+++ week06-07-jQuery/lab4/part5/choosen_multi_select/index.php
@@ -5,21 +5,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Five: Chosen Multi Select</title>
+    <title>Lab4 Part5: Multi Select JQuery Script</title>
     <script src="jquery.js"></script>
     <link rel="stylesheet" href="chosen.min.css">
     <script src="chosen.jquery.min.js"></script>
 </head>
 
 <body>
+    <h1>Lab4 Part5: Multi Select JQuery Script</h1>
+    <br/>
     <form action="" method="post">
         <select multiple name="skills[]" class="skills">
+            <option>HTML</option>
+            <option>CSS</option>
             <option>PHP</option>
             <option>jQuery</option>
             <option>AJAX</option>
             <option>JavaScript</option>
             <option>AngularJS</option>
             <option>Python</option>
+            <option>ASP</option>
+            <option>JAVA</option>
         </select>
         <input type="submit" name="submit" vlaue="SUBMIT">
     </form>
@@ -34,7 +40,7 @@
     if (isset($_POST['submit'])) {
         if (!empty($_POST['skills'])) {
             foreach ($_POST['skills'] as $selected) {
-                echo '  ' . $selected;
+                echo '  ' . $selected . " / ";
             }
         } else {
             echo 'Please select the value.';

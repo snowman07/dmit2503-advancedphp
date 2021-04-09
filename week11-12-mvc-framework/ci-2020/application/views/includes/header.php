4935b5b8921287206e43f80b1ea7bda24ef09b01
snowman07 <59627816+snowman07@users.noreply.github.com>
2021-04-09 11:35:13 -0600

    jQuery implementation

--- week11-12-mvc-framework/ci-2020/application/views/includes/header.php
+++ week11-12-mvc-framework/ci-2020/application/views/includes/header.php
@@ -107,7 +107,7 @@
  
     <?php $message = $this->session->flashdata('message'); ?> 
     <?php if ($message): ?> 
-        <h3 class="alert alert-primary"><i class="material-icons">thumb_up</i>&nbsp;<?php echo $message ?></h3> 
+        <h3 class="alert alert-primary" id="message"><i class="material-icons">thumb_up</i>&nbsp;<?php echo $message ?></h3> 
     <?php endif; ?>
 
     <!-- This ends the header include -->

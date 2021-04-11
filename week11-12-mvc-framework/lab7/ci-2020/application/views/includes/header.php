521d84dc0bfd74d45ca53c30817af5d8f9d2f476
snowman07 <59627816+snowman07@users.noreply.github.com>
2021-04-11 16:39:04 -0600

    header

--- week11-12-mvc-framework/lab7/ci-2020/application/views/includes/header.php
+++ week11-12-mvc-framework/lab7/ci-2020/application/views/includes/header.php
@@ -50,10 +50,9 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <!-- <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> -->
+                <!-- <li class="nav-item active">
                     <a class="navbar-brand" href="<?php echo base_url()?>"> <i class="material-icons">home</i></a>
-                </li>
+                </li> -->
                 <!-- <li class="nav-item">
                     <a class="nav-link" href="#">Features</a>
                 </li>
@@ -61,7 +60,7 @@
                     <a class="nav-link" href="#">Pricing</a>
                 </li> -->
                 <li class="nav-item active">
-                    <a class="navbar-brand" href="<?php echo base_url()?>crud">CRUDLINK</a>
+                    <a class="navbar-brand" href="<?php echo base_url()?>crud"><i class="material-icons">home</i></a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
